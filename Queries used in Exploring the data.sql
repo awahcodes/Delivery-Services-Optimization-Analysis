e2334555b013ac8aaa7cfe59d75c02dc6fb96323
awahcodes <awahcodes@gmail.com>
2025-01-05 16:26:06 +0000

    Further did transformations in power BI, added columns then created visualizations
    Included a date table with hierarchy to enable Time seies analysis

--- Queries used in Exploring the data.sql
+++ Queries used in Exploring the data.sql
@@ -1,42 +1,48 @@
-Query 1 - Identify frequently delivery route
+-- Query 1 - Identify frequently delivery route
 
-SELECT origin, destination, COUNT(*) as frequency
-FROM deliveries
-GROUP BY origin, destination
-ORDER BY frequency DESC
+-- SELECT origin, destination, COUNT(*) as frequency
+-- FROM deliveries
+-- GROUP BY origin, destination
+-- ORDER BY frequency DESC
 
-Query 2: Calculate Average Delivery Time per Route
+-- Query 2: Calculate Average Delivery Time per Route
 
-SELECT origin, destination, ROUND(AVG(deliverytime),0) as AverageDeliveryTime
-FROM deliveries
-GROUP BY origin, destination
-ORDER BY AverageDeliveryTime 
-
-
-3.	Query 3: Identify Costliest Routes
-
-Select r.origin, r.destination, SUM(c.totalcost) as totalsum
-FROM routes as r
-LEFT JOIN costs as c
-USING(routeid)
-GROUP BY r.origin, r.destination
-ORDER BY totalsum DESC
+-- SELECT origin, destination, ROUND(AVG(deliverytime),0) as AverageDeliveryTime
+-- FROM deliveries
+-- GROUP BY origin, destination
+-- ORDER BY AverageDeliveryTime 
 
 
-Query 4 - Detect Routes with Delays
+-- 3.	Query 3: Identify Costliest Routes
+
+-- Select r.origin, r.destination, SUM(c.totalcost) as totalsum
+-- FROM routes as r
+-- LEFT JOIN costs as c
+-- USING(routeid)
+-- GROUP BY r.origin, r.destination
+-- ORDER BY totalsum DESC
+
+
+-- Query 4 - Detect Routes with Delays
 SELECT origin, destination, deliverytime, expectedtime, deliverytime - expectedtime as delayedby
 FROM deliveries
 WHERE deliverytime > expectedtime
 
-Query 5 - Using conditional statement, express delayed, early or on time deliveries
+-- Query 5 - Using conditional statement, express delayed, early or on time deliveries
 
-SELECT deliveryid, origin, destination,
-CASE
- WHEN deliverytime > expectedtime THEN 'Delayed'
-	WHEN deliverytime = expectedtime THEN 'On time'
-	WHEN deliverytime < expectedtime THEN 'Early'
-END as deliverystatus
+-- SELECT deliveryid, origin, destination,
+-- CASE
+--  WHEN deliverytime > expectedtime THEN 'Delayed'
+-- 	WHEN deliverytime = expectedtime THEN 'On time'
+-- 	WHEN deliverytime < expectedtime THEN 'Early'
+-- END as deliverystatus
 
+-- FROM deliveries
+-- ORDER BY deliverystatus
+
+
+-- Query 6 -- Time series on deliveries
+
+SELECT date, origin, destination, deliverytime, expectedtime, deliverytime - expectedtime as delayedby
 FROM deliveries
-ORDER BY deliverystatus
 
